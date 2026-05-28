ab679cac7b4f9f3feb84a44f4f305412c0707445
moraisgomes123 <moraisgomesadaochabango@gmail.com>
2026-05-28 15:43:00 +0200

    Enhance Response.cs with property comments
    Added comments to clarify the purpose of properties.

--- CybersecurityChatbot/Models/Response.cs
+++ CybersecurityChatbot/Models/Response.cs
@@ -1,18 +1,22 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace CybersecurityChatbot.Models
 {
+    // Represents a chatbot response model
     public class Response
     {
+        // Main keyword used to identify the response
         public string Keyword { get; set; } = "";
 
-    public List<string> Synonyms { get; set; }
-        = new List<string>();
+        // List of alternative words related to the keyword
+        public List<string> Synonyms { get; set; }
+            = new List<string>();
 
+        // List of possible replies the chatbot can return
         public List<string> Replies { get; set; }
             = new List<string>();
 
+        // Single reply message
         public string Reply { get; set; } = "";
     }
-
 }
